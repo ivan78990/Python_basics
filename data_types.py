d3040bf5c040cca432046cbdbea8f2d870d6223c
warchang <warchang@student.21-school.ru>
2022-12-15 17:55:44 +0300

    data_types

---
+++ data_types.py
@@ -0,0 +1,9 @@
+message = "enter some value"
+days = 20
+price = 4.99
+valid_number = True
+exit_input = False
+list_of_days = [20, 40, 100, 20]
+list_of_months = ["January", "February"]
+set_of_days = {20, 49, 100 }  # no dublicates
+days_and_unit_dictionary = {"days": 10, "unit": "hours"} # key: value
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-to_unit = 24
-name_of_unit = "hours"
 
-
-def days_to_units(num_of_days):
-    return f"{num_of_days} days are {num_of_days * to_unit} {name_of_unit}"
+def days_to_units(num_of_days, conversion_unit):
+    if conversion_unit == "hours":
+        return f"{num_of_days} days are {num_of_days * 24} hours"
+    elif conversion_unit == "minutes":
+        return f"{num_of_days} days are {num_of_days * 24 * 60} minutes"
+    else:
+        return "unsupported unit"
 
 
 def validate_and_execute():
     try:
-        user_input_number = int(num_of_days_element)
+        user_input_number = int(days_and_unit_dictionary["days"])
 
         # we want to do conversion only for positive integers
         if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number)
+            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
             print(calculated_value)
         elif user_input_number == 0:
             print("It's zero!")
@@ -26,8 +28,23 @@ user_input = ""
 while user_input != "exit":
     user_input = input("Conversion!\n")
     days_and_unit = user_input.split(":")
+    days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
     print(days_and_unit)
-    days_and_unit_dictionary = {"days": days_and_unit[0], "unit": "hours"}
+    print(days_and_unit_dictionary)
+    print(type(days_and_unit_dictionary))
+    validate_and_execute()
+
+
+
+
+
+# my_list = ["20", "30"]
+# print(my_list[1])
+#
+# my_dictionary = {"days": 20, "unit": "hours", "message": "all good" }
+# print(my_dictionary["days"])
+# print(my_dictionary["unit"])
+# print(my_dictionary["message"])
 
     # print(list_of_days)
     # print(set(list_of_days))
